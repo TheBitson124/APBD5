75f80ac8cfaf4737801e654b28c7c08a3d66486c
TheBitson124 <as.sosnowicz@gmail.com>
2024-04-11 14:01:36 +0100

    Animal Model

--- project5/project5/Models/Animal.cs
+++ project5/project5/Models/Animal.cs
@@ -4,4 +4,8 @@ public class Animal
 {
     public int Id { get; set; }
     public string FirstName { get; set; }
+    public string Category { get; set; }
+    public int Mass { get; set; }
+    public string FurColor { get; set; }
+
 }
